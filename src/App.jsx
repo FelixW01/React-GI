9469e79c3199905f210f0f83dda28f8f63ce7065
FelixW01 <felixwillem01@yahoo.com>
2025-01-16 17:10:39 -0500

    initial commit:

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  // Class App {
+  //   constructor()
+  // }
+  return (
+    <div>
+      <h2>name</h2>
+      <p>Address</p>
+      <p>Date of birth</p>
+      <p>Alignment: Lawful good</p>
+    </div>
+  )
+}
+
+export default App
