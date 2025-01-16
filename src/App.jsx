04de7c9699cffb1401c04f4686948f7c1a635229
FelixW01 <felixwillem01@yahoo.com>
2025-01-16 17:55:36 -0500

    worked on app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import './App.css'
 
 function App() {
-  // Class App {
-  //   constructor()
-  // }
+  class App {
+    constructor(props) {
+      this.state = {}
+    }
+      render() {
+        return (
+        <div>
+            <h2>name</h2>
+            <p>Address</p>
+            <p>Date of birth</p>
+            <p>Alignment: Lawful good</p>
+        </div>
+      )
+    }
+  }
+  const newApp = new App(props)
+
   return (
     <div>
       <h2>name</h2>
