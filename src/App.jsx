1e1ebea76bc692bbb7bd84160d131010a7ed1897
Felix W <felixwillem01@yahoo.com>
2025-01-16 19:07:39 -0500

    finished working on rendering contact list

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,71 @@ import './App.css'
 // App class, render method with a hard coded div  *
 // Make div dynamic by using this.state 
 // Take in props from parent component
+
+// BasicInfo class component that takes in info from App and renders it dynamically
+class BasicInfo extends Component {
+    render() {
+      const { name, phone, dob, email } = this.props.person;
+      return (
+        <div>
+           <h2>{name}</h2>
+           <p>Phone: {phone}</p>
+           <p>Date of Birth: {dob}</p>
+           <p>Email: {email}</p>
+        </div>
+      );
+    }
+  };
   class App extends Component {
     constructor(props) {
       super(props);
       this.state = {
-        person: {
-          name: props.name,
-          phone: props.phone,
-          dob: props.dob,
-          email: props.email
-        }
+        people: [
+          {
+          name: 'Felix Willem',
+          phone: '(212) 123-4567',
+          dob: 'Feb 2, 2000',
+          email: 'felix@yahoo.com'
+        },
+        {
+          name: 'Kobe Bryant',
+          phone: '(212) 123-4527',
+          dob: 'Feb 2, 2000',
+          email: 'ron@yahoo.com'
+        },
+        {
+          name: 'Lionel Messi',
+          phone: '(212) 123-4577',
+          dob: 'Feb 2, 2000',
+          email: 'mes@yahoo.com'
+        },
+        {
+          name: 'Bob the Coder',
+          phone: '(212) 123-3567',
+          dob: 'Feb 2, 2000',
+          email: 'bob@yahoo.com'
+        },
+      ]
       };
     }
       render() {
         return (
-        <div>
-          <h1>Name: {this.state.person.name}</h1>
-          <p>Phone: {this.state.person.phone}</p>
-          <p>Date of Birth: {this.state.person.dob}</p>
-          <p>Email: {this.state.person.email}</p>
+        <div className='div-container'>
+        <h1>Contact</h1>
+        <div className='hero-div'>
+        {this.state.people.map((person, index) => {
+            return (
+              <div key={index} className='card'>
+              <BasicInfo person={person} />
+              </div>
+              )
+        })}
+        </div>
         </div>
       )
     }
   }
-  
+
+ 
 export default App;
+      
--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App 
-      name="felix Willem"
-      phone="(212) 123-4567"
-      dob="Feb 2, 2000"
-      email="felix@yahoo.com"
-    />
+    <App />
   </StrictMode>,
 )
